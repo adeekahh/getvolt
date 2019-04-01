355b3c51ede68643e60cd817a43771b4cf7f6c49
adeekahh <ekydesignlab@gmail.com>
2019-04-01 12:19:05 +0200

    gif referral

---
+++ main.js
@@ -0,0 +1,33 @@
+import lax from "lax.js";
+
+window.onload = function() {
+  lax.setup(); // init
+
+  const updateLax = () => {
+    lax.update(window.scrollY);
+    window.requestAnimationFrame(updateLax);
+  };
+
+  window.requestAnimationFrame(updateLax);
+};
+
+window.onscroll = function() {
+  progressBar();
+};
+
+function progressBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.querySelector(".battery-level").style.height = scrolled + "%";
+  console.log(scrolled);
+  if (scrolled > 25 && scrolled < 75) {
+    document.querySelector(".battery-level").style.backgroundColor = "orange";
+  } else if (scrolled > 75) {
+    document.querySelector(".battery-level").style.backgroundColor = "green";
+  } else {
+    document.querySelector(".battery-level").style.backgroundColor = "red";
+  }
+}
